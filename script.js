4004048034a4f84b734d6889f55ecfa29c416fea
AnuarZhurkayev <166802822+AnuarZhurkayev@users.noreply.github.com>
2024-05-22 15:26:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 
-// Функция для обработки нажатия на кнопку "Нравится"
-function likePost(event) {
-    const likeButton = event.target;
-    const likeCount = likeButton.parentNode.querySelector(".like-count");
-    let currentLikes = parseInt(likeCount.textContent);
-    likeCount.textContent = currentLikes + 1;
-}
-
-// Получаем все кнопки "Нравится" на странице
-const likeButtons = document.querySelectorAll('.like-btn');
-likeButtons.forEach(button => {
-    button.addEventListener('click', likePost);
-});
-
 });
 function commentPost(event) {
     const commentButton = event.target;
