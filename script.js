0a6c0390dad7759766dbefba847b95f42231f7f0
AnuarZhurkayev <166802822+AnuarZhurkayev@users.noreply.github.com>
2024-05-22 14:50:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
-        const commentInput = document.querySelector('.comment-input:focus');
-        if (commentInput) {
-            const commentText = commentInput.value;
-            console.log('Комментирование поста:', commentText);
-            commentInput.value = '';
+document.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        const c = document.querySelector('.comment-input:focus');
+        if (c) {
+            const t = c.value;
+            console.log('Коммент:', t);
+            c.value = '';
         }
     }
 });
