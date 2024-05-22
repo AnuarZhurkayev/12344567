969e1e47f2f49f23e05f4ebc7e89e79e5de8473f
AnuarZhurkayev <166802822+AnuarZhurkayev@users.noreply.github.com>
2024-05-22 14:55:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Функция для обработки нажатия на кнопку "Нравится"
+
 function likePost(event) {
     const likeButton = event.target;
     const likeCount = likeButton.parentNode.querySelector('.like-count');
@@ -6,27 +6,27 @@ function likePost(event) {
     likeCount.textContent = currentLikes + 1;
 }
 
-// Получаем все кнопки "Нравится" на странице
+
 const likeButtons = document.querySelectorAll('.like-btn');
 likeButtons.forEach(button => {
     button.addEventListener('click', likePost);
 });
 
-// Функция для обработки нажатия на кнопку "Комментировать"
+
 function commentPost(event) {
     const commentButton = event.target;
     const post = commentButton.closest('.post');
     const commentInput = post.querySelector('.comment-input');
     const commentText = commentInput.value;
     console.log('Комментирование поста:', commentText);
-    commentInput.value = ''; // Сброс поля ввода после комментирования
+    commentInput.value = ''; 
 }
 
-// Получаем все поля ввода для комментариев на странице
+
 const commentInputs = document.querySelectorAll('.comment-input');
 commentInputs.forEach(input => {
     input.addEventListener('keypress', function(event) {
-        if (event.key === 'Enter') { // Проверяем нажатие клавиши Enter
+        if (event.key === 'Enter') { 
             commentPost(event);
         }
     });
